784c2c65230ab764fe7e0f759107d5d1fa20ee1f
PradatiusD <danielprada2012@gmail.com>
2014-04-02 15:02:10 -0400

    Added mailchimp e-mail singup form

--- issst/front-page.php
+++ issst/front-page.php
@@ -14,30 +14,270 @@
 		</div>
 	</div>
 </section>
-<section>	
-	<div class="container">
-		<div class="row">
-			<div class="col-md-8">
-				<h1>Welcome to ISSST</h1>
-				<p class="lead">The International Symposium on Sustainable Systems and Technology (ISSST) is the premier conference for research related to the sustainability of science and technology systems. The program covers the spectrum of issues for assessing and managing products and services across their life cycle, and the design, management, and policy implications of sustainable engineered systems and technologies.</p>
-				<p><strong>Authors</strong> are invited to submit papers describing research, applications, tools, and case studies addressing the topics below. Proceedings will be distributed exclusively to the conference participants and authors may retain copyrights to the submitted papers, or elect to submit their papers for consideration in sustainability-related journals.</p>
-				<p><strong>Students</strong> are especially encouraged to attend and can participate in special paper and poster competitions, judged by leading academics and researchers from government and industry.</p>
-				<p>ISSST is entering its 20th successful year.  Between 1993 and 2008, the conference was known as the IEEE International Symposium on Electronics and the Environment (IEEE ISEE). In 2008, however, the scope of the conference was broadened to all industries and products while still maintaining a focus on the sustainability of information and communication technology (ICT) services. The new scope further strengthens this premier event on sustainable technology issues. Accordingly, the name has been changed to International Symposium on Sustainable Systems and Technology (ISSST).</p>
-			</div>
-			<div class="col-md-3 pasta">
-			</div>
+<div class="container">
+	<section class="row">
+		<div class="col-md-8">
+			<h1>Welcome to ISSST</h1>
+			<p class="lead">The International Symposium on Sustainable Systems and Technology (ISSST) is the premier conference for research related to the sustainability of science and technology systems. The program covers the spectrum of issues for assessing and managing products and services across their life cycle, and the design, management, and policy implications of sustainable engineered systems and technologies.</p>
+			<p><strong>Authors</strong> are invited to submit papers describing research, applications, tools, and case studies addressing the topics below. Proceedings will be distributed exclusively to the conference participants and authors may retain copyrights to the submitted papers, or elect to submit their papers for consideration in sustainability-related journals.</p>
+			<p><strong>Students</strong> are especially encouraged to attend and can participate in special paper and poster competitions, judged by leading academics and researchers from government and industry.</p>
+			<p>ISSST is entering its 20th successful year.  Between 1993 and 2008, the conference was known as the IEEE International Symposium on Electronics and the Environment (IEEE ISEE). In 2008, however, the scope of the conference was broadened to all industries and products while still maintaining a focus on the sustainability of information and communication technology (ICT) services. The new scope further strengthens this premier event on sustainable technology issues. Accordingly, the name has been changed to International Symposium on Sustainable Systems and Technology (ISSST).</p>
 		</div>
-		<div class="row">
-			<div class="col-md-6">
-				<div id="map-canvas"></div>
-			</div>
-			<div class="col-md-6">
-				<h1>Welcome to Oakland</h1>
-				<p class="lead">This year’s conference will be held in Oakland, California at the Marriott City Center from 19 May – 21 May, 2014, with a pre-conference workshop being held on May 18th, and post-conference workshops being held on May 22nd. Information on registration and the schedule will be available soon. If you have any questions, direct them to ISSSTNetwork@gmail.com.</p>
-			</div>
+		<div class="col-md-3 pasta">
 		</div>
-	</div>
-</section>
+	</section>
+	<section class="row">
+		<div class="col-md-6">
+			<div id="map-canvas"></div>
+		</div>
+		<div class="col-md-6">
+			<h1>Welcome to Oakland</h1>
+			<p class="lead">This year’s conference will be held in Oakland, California at the Marriott City Center from 19 May – 21 May, 2014, with a pre-conference workshop being held on May 18th, and post-conference workshops being held on May 22nd. Information on registration and the schedule will be available soon. If you have any questions, direct them to ISSSTNetwork@gmail.com.</p>
+		</div>
+	</section>
+	<section class="row">
+		<div class="col-md-6 col-md-offset-3">
+			<h1>Want to get PISSST weekly?</h1>
+			<p class="lead">Sign up to be part of Proceedings from the International Symposium on Sustainable Systems & Technology, better known as PISSST.</p>
+
+			<!-- Begin MailChimp Signup Form -->
+			<div id="mc_embed_signup">
+				<form action="http://wordpress.us6.list-manage1.com/subscribe/post?u=9c4c3f7f77bdb09de3fa52e0a&amp;id=8f5c8a5f2d" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+					<div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+					<div class="mc-field-group">
+						<label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
+					</label>
+						<input type="email" value="" name="EMAIL" class="required email form-control" id="mce-EMAIL">
+					</div>
+					<br>
+					<div class="row">
+						<div class="mc-field-group col-md-6">
+							<label for="mce-FNAME">First Name </label>
+							<input type="text" value="" name="FNAME" class="form-control" id="mce-FNAME">
+						</div>
+						<div class="mc-field-group col-md-6">
+							<label for="mce-LNAME">Last Name </label>
+							<input type="text" value="" name="LNAME" class="form-control" id="mce-LNAME">
+						</div>
+					</div>
+					<br>
+					<div class="mc-field-group input-group">
+					    <strong>Email Format </strong>
+					    <ul>
+					    	<li>
+					    		<input type="radio" value="html" name="EMAILTYPE" id="mce-EMAILTYPE-0"><label for="mce-EMAILTYPE-0">html</label>
+					    	</li>
+						<li>
+							<input type="radio" value="text" name="EMAILTYPE" id="mce-EMAILTYPE-1"><label for="mce-EMAILTYPE-1">text</label>
+						</li>
+					</ul>
+					</div>
+						<div id="mce-responses" class="clear">
+							<div class="response" id="mce-error-response" style="display:none"></div>
+							<div class="response" id="mce-success-response" style="display:none"></div>
+						</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+					    <div style="position: absolute; left: -5000px;"><input type="text" name="b_9c4c3f7f77bdb09de3fa52e0a_8f5c8a5f2d" value=""></div>
+						<div class="clear">
+							<input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="btn btn-primary btn-lg">
+						</div>
+				</form>
+			</div>
+			<script type="text/javascript">
+			var fnames = new Array();var ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';
+			try {
+			    var jqueryLoaded=jQuery;
+			    jqueryLoaded=true;
+			} catch(err) {
+			    var jqueryLoaded=false;
+			}
+			var head= document.getElementsByTagName('head')[0];
+			if (!jqueryLoaded) {
+			    var script = document.createElement('script');
+			    script.type = 'text/javascript';
+			    script.src = '//ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js';
+			    head.appendChild(script);
+			    if (script.readyState && script.onload!==null){
+			        script.onreadystatechange= function () {
+			              if (this.readyState == 'complete') mce_preload_check();
+			        }    
+			    }
+			}
+
+			var err_style = '';
+			try{
+			    err_style = mc_custom_error_style;
+			} catch(e){
+			    err_style = '#mc_embed_signup input.mce_inline_error{border-color:#6B0505;} #mc_embed_signup div.mce_inline_error{margin: 0 0 1em 0; padding: 5px 10px; background-color:#6B0505; font-weight: bold; z-index: 1; color:#fff;}';
+			}
+			var head= document.getElementsByTagName('head')[0];
+			var style= document.createElement('style');
+			style.type= 'text/css';
+			if (style.styleSheet) {
+			  style.styleSheet.cssText = err_style;
+			} else {
+			  style.appendChild(document.createTextNode(err_style));
+			}
+			head.appendChild(style);
+			setTimeout('mce_preload_check();', 250);
+
+			var mce_preload_checks = 0;
+			function mce_preload_check(){
+			    if (mce_preload_checks>40) return;
+			    mce_preload_checks++;
+			    try {
+			        var jqueryLoaded=jQuery;
+			    } catch(err) {
+			        setTimeout('mce_preload_check();', 250);
+			        return;
+			    }
+			    var script = document.createElement('script');
+			    script.type = 'text/javascript';
+			    script.src = 'http://downloads.mailchimp.com/js/jquery.form-n-validate.js';
+			    head.appendChild(script);
+			    try {
+			        var validatorLoaded=jQuery("#fake-form").validate({});
+			    } catch(err) {
+			        setTimeout('mce_preload_check();', 250);
+			        return;
+			    }
+			    mce_init_form();
+			}
+			function mce_init_form(){
+			    jQuery(document).ready( function($) {
+			      var options = { errorClass: 'mce_inline_error', errorElement: 'div', onkeyup: function(){}, onfocusout:function(){}, onblur:function(){}  };
+			      var mce_validator = $("#mc-embedded-subscribe-form").validate(options);
+			      $("#mc-embedded-subscribe-form").unbind('submit');//remove the validator so we can get into beforeSubmit on the ajaxform, which then calls the validator
+			      options = { url: 'http://wordpress.us6.list-manage.com/subscribe/post-json?u=9c4c3f7f77bdb09de3fa52e0a&id=8f5c8a5f2d&c=?', type: 'GET', dataType: 'json', contentType: "application/json; charset=utf-8",
+			                    beforeSubmit: function(){
+			                        $('#mce_tmp_error_msg').remove();
+			                        $('.datefield','#mc_embed_signup').each(
+			                            function(){
+			                                var txt = 'filled';
+			                                var fields = new Array();
+			                                var i = 0;
+			                                $(':text', this).each(
+			                                    function(){
+			                                        fields[i] = this;
+			                                        i++;
+			                                    });
+			                                $(':hidden', this).each(
+			                                    function(){
+			                                        var bday = false;
+			                                        if (fields.length == 2){
+			                                            bday = true;
+			                                            fields[2] = {'value':1970};//trick birthdays into having years
+			                                        }
+			                                    	if ( fields[0].value=='MM' && fields[1].value=='DD' && (fields[2].value=='YYYY' || (bday && fields[2].value==1970) ) ){
+			                                    		this.value = '';
+												    } else if ( fields[0].value=='' && fields[1].value=='' && (fields[2].value=='' || (bday && fields[2].value==1970) ) ){
+			                                    		this.value = '';
+												    } else {
+												        if (/\[day\]/.test(fields[0].name)){
+			    	                                        this.value = fields[1].value+'/'+fields[0].value+'/'+fields[2].value;									        
+												        } else {
+			    	                                        this.value = fields[0].value+'/'+fields[1].value+'/'+fields[2].value;
+				                                        }
+				                                    }
+			                                    });
+			                            });
+			                        $('.phonefield-us','#mc_embed_signup').each(
+			                            function(){
+			                                var fields = new Array();
+			                                var i = 0;
+			                                $(':text', this).each(
+			                                    function(){
+			                                        fields[i] = this;
+			                                        i++;
+			                                    });
+			                                $(':hidden', this).each(
+			                                    function(){
+			                                        if ( fields[0].value.length != 3 || fields[1].value.length!=3 || fields[2].value.length!=4 ){
+			                                    		this.value = '';
+												    } else {
+												        this.value = 'filled';
+				                                    }
+			                                    });
+			                            });
+			                        return mce_validator.form();
+			                    }, 
+			                    success: mce_success_cb
+			                };
+			      $('#mc-embedded-subscribe-form').ajaxForm(options);
+			      
+			      
+			    });
+			}
+			function mce_success_cb(resp){
+			    $('#mce-success-response').hide();
+			    $('#mce-error-response').hide();
+			    if (resp.result=="success"){
+			        $('#mce-'+resp.result+'-response').show();
+			        $('#mce-'+resp.result+'-response').html(resp.msg);
+			        $('#mc-embedded-subscribe-form').each(function(){
+			            this.reset();
+			    	});
+			    } else {
+			        var index = -1;
+			        var msg;
+			        try {
+			            var parts = resp.msg.split(' - ',2);
+			            if (parts[1]==undefined){
+			                msg = resp.msg;
+			            } else {
+			                i = parseInt(parts[0]);
+			                if (i.toString() == parts[0]){
+			                    index = parts[0];
+			                    msg = parts[1];
+			                } else {
+			                    index = -1;
+			                    msg = resp.msg;
+			                }
+			            }
+			        } catch(e){
+			            index = -1;
+			            msg = resp.msg;
+			        }
+			        try{
+			            if (index== -1){
+			                $('#mce-'+resp.result+'-response').show();
+			                $('#mce-'+resp.result+'-response').html(msg);            
+			            } else {
+			                err_id = 'mce_tmp_error_msg';
+			                html = '<div id="'+err_id+'" style="'+err_style+'"> '+msg+'</div>';
+			                
+			                var input_id = '#mc_embed_signup';
+			                var f = $(input_id);
+			                if (ftypes[index]=='address'){
+			                    input_id = '#mce-'+fnames[index]+'-addr1';
+			                    f = $(input_id).parent().parent().get(0);
+			                } else if (ftypes[index]=='date'){
+			                    input_id = '#mce-'+fnames[index]+'-month';
+			                    f = $(input_id).parent().parent().get(0);
+			                } else {
+			                    input_id = '#mce-'+fnames[index];
+			                    f = $().parent(input_id).get(0);
+			                }
+			                if (f){
+			                    $(f).append(html);
+			                    $(input_id).focus();
+			                } else {
+			                    $('#mce-'+resp.result+'-response').show();
+			                    $('#mce-'+resp.result+'-response').html(msg);
+			                }
+			            }
+			        } catch(e){
+			            $('#mce-'+resp.result+'-response').show();
+			            $('#mce-'+resp.result+'-response').html(msg);
+			        }
+			    }
+			}
+
+			</script>
+			<!--End mc_embed_signup-->
+		</div>
+
+	</section>
+</div>
 
 <div id="maximage">
 	<img src="http://localhost/issst/wp-content/themes/issst/img/slide1.jpg" alt="">
