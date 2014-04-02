3e7259cb2958cc4d9df2cd590bd2f39270b48155
PradatiusD <danielprada2012@gmail.com>
2014-04-02 19:40:27 -0400

    Added footer and more slides

--- issst/footer.php
+++ issst/footer.php
@@ -1,4 +1,17 @@
 		<?php wp_footer();?>
+		<footer>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8">
+						<h3>About the name</h3>
+						<p>ISSST is entering its 20th successful year.  Between 1993 and 2008, the conference was known as the IEEE International Symposium on Electronics and the Environment (IEEE ISEE). In 2008, however, the scope of the conference was broadened to all industries and products while still maintaining a focus on the sustainability of information and communication technology (ICT) services. The new scope further strengthens this premier event on sustainable technology issues. Accordingly, the name has been changed to International Symposium on Sustainable Systems and Technology (ISSST).</p>
+					</div>
+					<div class="col-md-4">
+						<img src="http://localhost/issst/wp-content/themes/issst/img/issst.png" class="img-responsive" alt="">
+					</div>
+				</div>		
+			</div>
+		</footer>
 	</body>
 	<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/lib/collapse.js"></script>
 	<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/lib/transition.js"></script>
--- issst/front-page.php
+++ issst/front-page.php
@@ -18,7 +18,7 @@
 	<section class="row">
 		<div class="col-md-8">
 			<h1>Welcome to ISSST</h1>
-			<p class="lead">The <strong>International Symposium on Sustainable Systems and Technology</strong> (ISSST) is the premier conference for research related to the sustainability of science and technology systems. It covers the spectrum of issues for assessing and managing products and services across their life cycle, and the design, management, and policy implications of sustainable engineered systems and technologies.</p>
+			<p class="lead">The <strong>International Symposium on Sustainable Systems and Technology</strong> (ISSST) covers the spectrum of issues for assessing and managing products and services across their life cycle, and the design, management, and policy implications of sustainable engineered systems and technologies.</p>
 			<div class="row">
 				<div class="col-md-6">
 					<p><strong>Authors</strong> are invited to submit papers describing research, applications, tools, and case studies addressing the topics below. Proceedings will be distributed exclusively to the conference participants and authors may retain copyrights to the submitted papers, or elect to submit their papers for consideration in sustainability-related journals.</p>				
@@ -27,9 +27,8 @@
 					<p><strong>Students</strong> are especially encouraged to attend and can participate in special paper and poster competitions, judged by leading academics and researchers from government and industry.</p>
 				</div>				
 			</div>
-			<p>ISSST is entering its 20th successful year.  Between 1993 and 2008, the conference was known as the IEEE International Symposium on Electronics and the Environment (IEEE ISEE). In 2008, however, the scope of the conference was broadened to all industries and products while still maintaining a focus on the sustainability of information and communication technology (ICT) services. The new scope further strengthens this premier event on sustainable technology issues. Accordingly, the name has been changed to International Symposium on Sustainable Systems and Technology (ISSST).</p>
 		</div>
-		<div class="col-md-3 pasta">
+		<div class="col-md-4 pasta">
 		</div>
 	</section>
 	<section class="row">
@@ -38,11 +37,179 @@
 		</div>
 		<div class="col-md-6">
 			<h1>Welcome to Oakland</h1>
-			<p class="lead">This year’s conference will be held in Oakland, California at the Marriott City Center from 19 May – 21 May, 2014.</p>
+			<p class="lead">This year’s conference will be held in Oakland, California at the Marriott City Center from <strong>19-21 May, 2014</strong>.</p>
 			<p>A pre-conference workshop will be held on May 18th, and a post-conference workshops will be held on May 22nd.</p>
 			<p>Information on registration and the schedule will be available soon. If you have any questions, direct them to <a href="mailto:ISSSTNetwork@gmail.com">ISSSTNetwork@gmail.com</a>.</p>
 			<p><a href="https://www.regonline.com/builder/site/?eventid=1482855" class="btn btn-primary btn-large btn-success btn-block register-now">Register Now!</a></p>
 		</div>
+	</section>
+	<section class="row">
+		<h1>Monstrous Schedule</h1>
+		<table class="table table-striped">
+			<thead>
+				<tr>
+					<th style="width: 72px">Time</th>
+					<th class="text-center">Track 1</th>
+					<th class="text-center">Track 2</th>
+					<th class="text-center">Track 3</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td colspan="4"><h2>Sunday May 18th</h2></td>
+				</tr>
+				<tr>
+					<td>09:00 - 12:00</td>
+					<td colspan="3" class="text-center">
+						<strong>Onsite registration</strong>
+					</td>					
+				</tr>
+				<tr>
+					<td>13:00 - 17:00</td>
+					<td colspan="3" class="text-center">
+						<strong>Pre-conference sessions (to be added)</strong>
+					</td>					
+				</tr>
+				<tr>
+					<td colspan="4"><h2>Monday May 19th</h2></td>
+				</tr>
+				<tr>
+					<td>13:00 - 14:00</td>
+					<td colspan="3" class="text-center">
+						<strong>Welcome:</strong> Tom Seager, <em>ISSST 2014 Conference Chair</em><br>
+						<strong>Opening Plenary:</strong> Capt. Wayne Porter, <em>US Navy Post-graduate School</em>
+					</td>					
+				</tr>
+				<tr>
+					<td>14:00 - 15:30</td>
+					<td class="success">Be Careful What You Ask For: How Corporate Average Fuel Economy Incentivizing Alternative Fuel Vehicles Leads to Higher Carbon Emissions</td>
+					<td class="warning">Reuse and Refurbishment Concept for Cost-Effective Treatment of Increasing Amounts of Defective Solar Modules</td>
+					<td class="info">Improving Metropolitan Climate Action Planning by Incorporating Omissions and Quantifying Uncertainties</td>
+				</tr>
+				<tr>
+					<td></td>
+					<td class="success">Transit-oriented Smart Growth can Reduce Life-cycle Environmental Impacts and Household Energy and Travel Costs in Los Angeles</td>
+					<td class="warning">Environmental trade-offs between utilizing recycled and virgin Tellurium for CdTe photovoltaic modules: Using anticipatory Life Cycle Assessment to analyze favorable pathways for future CdTe deployments</td>
+					<td class="info">Building A Dynamic Multi-City, Regional GHG Emissions for the Phoenix Metropolitan Area</td>
+				</tr>
+				<tr>
+					<td></td>
+					<td class="success">A Comparison of Air Emissions from Natural Gas Pathways for Road Transportation</td>
+					<td class="warning">GIS based environmental impact assessment of Photovoltaics in the Southeast region</td>
+					<td class="info">The SuRe (Sustainable and Resilient) Zone of Urban Infrastructure System Planning & Design</td>
+				</tr>
+				<tr>
+					<td></td>
+					<td class="success">Global Demand Forecast for Precious Group Metals (PGM) in the Automobile Industry</td>
+					<td class="warning">Design of Sustainable, Efficient, Safe and Economically Viable Solution for Solar Street Lighting System</td>
+					<td class="info">Complementing Urban Metabolism with Life-cycle Assessment for Sustainable Communities: A Case Study of the Phoenix Transportation System</td>
+				</tr>
+				<tr>
+					<td>15:30 - 16:00</td>
+					<td colspan="3" class="text-center">
+						<strong>Break</strong>
+					</td>					
+				</tr>
+				<tr>
+					<td>16:00 - 17:30</td>
+					<td class="success">Environmental tradeoffs and ethical dilemmas from increasing switchgrass biofuel production to meet current U.S. Renewable Fuel Standard and the CA low carbon biofuel standards</td>
+					<td class="warning">Assessing Regional Differences in Lighting Heat Replacement Effects in Residential Buildings across the United States</td>
+					<td class="info">Analysis of New Egyptian Cities Towards Sustainable Urban Metabolism</td>
+				</tr>
+				<tr>
+					<td></td>
+					<td class="success">Two-Stage Valorization of Cassava Processing Wastes into Biofuels</td>
+					<td class="warning">Environmental and Economic Impacts of On-Site Renewable Energy for Buildings</td>
+					<td class="info">How to make pre-chemicals out of municipal solid waste in an innovative, lucrative and environment-friendly way</td>
+				</tr>
+				<tr>
+					<td></td>
+					<td class="success">Quantifying the Carbon-Water-Energy Nexus for Drop-in Replacement Biofuels Produced from Perennial Grasses</td>
+					<td class="warning">A Meta-Analysis of Research on Healthy Buildings and Employee Productivity</td>
+					<td class="info">Identifying the scale and nexus of Carbon, Nitrogen and Biodiversity Impacts of Urban Systems.</td>
+				</tr>
+				<tr>
+					<td></td>
+					<td class="success">Greenhouse Gas Mitigation Benefits of Expanding U.S. Biofuel Incentives to Promote Biomass Use in Chemical Feedstocks</td>
+					<td class="warning">Comparison of empirical and simulated models for residential electricity consumption</td>
+					<td class="info">System Assessment of Water and Wastewater Services</td>
+				</tr>
+				<tr>
+					<td>17:30 - 19:30</td>
+					<td colspan="3" class="text-center">
+						<strong>POSTER SESSION: Refreshments served.</strong>
+					</td>					
+				</tr>
+				<tr>
+					<td colspan="4"><h2>Tuesday May 20th</h2></td>
+				</tr>
+				<tr>
+					<td>7:30-8:30</td>
+					<td colspan="3" class="text-center">
+						<strong>BREAKFAST: fruit juice, coffee, danish & bagels.  STUDENT ORGANIZING COMMITTEE MEETING</strong>
+					</td>					
+				</tr>
+				<tr>
+					<td>08:30 - 10:00</td>
+					<td class="success">Assessing the Costs of Policies to Achieve Alternative Fuel Goals using State-level Modeling Methods</td>
+					<td class="warning">Life Cycle Greenhouse Gas Implications of Exporting LNG to Meet China’s Growing Energy Demand</td>
+					<td class="info">Society in Sustainability Transitions: Intersecting Life Cycle Assessment Results with Institutional Analysis as a Method for Achieving Sustainability Interventions</td>
+				</tr>
+				<tr>
+					<td></td>
+					<td class="success">Implications of market-mediated emissions and uncertainty for biofuel policies</td>
+					<td class="warning">Life Cycle Assessment of the Use of Forest Biomass for Electricity</td>
+					<td class="info">Understanding Sustainable Technology and Human Behaviour: Adolescence to Adulthood!</td>
+				</tr>
+				<tr>
+					<td></td>
+					<td class="success">Buying greener electronics: A global review of purchasing policies and practices</td>
+					<td class="warning">Life Cycle Implications of Vehicle Lightweighting: A Case Study Based on a Physics-based Model</td>
+					<td class="info">Variability and uncertainty in social cost estimates for air quality</td>
+				</tr>
+				<tr>
+					<td></td>
+					<td class="success">Factors Governing Change in Water Withdrawals for U.S. Industrial Sectors</td>
+					<td class="warning">To be announced</td>
+					<td class="info">Indirect Impacts of Renewable Penetration and the Growing Importance of Life Cycle Assessment</td>
+				</tr>
+				<tr>
+					<td></td>
+					<td class="success">Markets for used electronics parts, the missing component in the e-waste management systems.</td>
+					<td class="warning">Environmental trade-offs of electric vehicle lithium-ion batteries with a cascading second life</td>
+					<td class="info">Understanding the role of use phase on improving the environmental sustainability of pavements</td>
+				</tr>
+				<tr>
+					<td colspan="4"><h2>Wednesday May 21th</h2></td>
+				</tr>
+				<tr>
+					<td>8:30 - 10:00</td>
+					<td class="success">Life-Cycle Inventory and Energy Analysis of FinFET Integrated Circuits</td>
+					<td class="warning">Estimating the carbon balance of hydropower development in the Brazilian Legal Amazon</td>
+					<td class="info">Measuring resilience through metrics: A comparative study of resilience metrics for complex systems</td>
+				</tr>
+				<tr>
+					<td></td>
+					<td class="success">Energy and carbon footprint performance metrics based on LCA in the ICT sector</td>
+					<td class="warning">Land and Nutrient Reclamation and Optimization for Spatially-Informed Renewable Energy Production</td>
+					<td class="info">Comparative Analysis of Sustainability Trends Determined by the Inclusive Wealth Index vs. Traditional Metrics</td>
+				</tr>
+				<tr>
+					<td></td>
+					<td class="success">Conflict minerals in the computer sector: Estimating the global use of Sn, Ta, W, Au in selected ICT products.</td>
+					<td class="warning">Cereal rotation and the use of a Decision Support System to improve the sustainability of durum wheat cultivation</td>
+					<td class="info">Comparing NFA and MRIO approaches for calculating nations' Ecological Footprint of consumption</td>
+				</tr>
+<!-- 
+				<tr>
+					<td></td>
+					<td class="success"></td>
+					<td class="warning"></td>
+					<td class="info"></td>
+				</tr>
+ -->
+			</tbody>
+		</table>
 	</section>
 	<section class="row">
 		<div class="col-md-6 col-md-offset-3 mailchimp-join-list">
@@ -293,10 +460,12 @@
 </div>
 <div id="maximage">
 	<img src="<?php bloginfo('template_url'); ?>/img/slide1.jpg" alt="">
+	<img src="<?php bloginfo('template_url'); ?>/img/slide2.jpg" alt="">
 	<img src="<?php bloginfo('template_url'); ?>/img/slide3.jpg" alt="">
 	<img src="<?php bloginfo('template_url'); ?>/img/slide4.jpg" alt="">
 	<img src="<?php bloginfo('template_url'); ?>/img/slide5.jpg" alt="">
 	<img src="<?php bloginfo('template_url'); ?>/img/slide6.jpg" alt="">
+	<img src="<?php bloginfo('template_url'); ?>/img/slide7.jpg" alt="">
 </div>
 
 <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
@@ -355,7 +524,8 @@ function initialize() {
 			'<h4>Marriott City Center</h4>'+
 			'<div id="bodyContent">'+
 				'<b>Address</b>: 1001 Broadway, Oakland, CA 94607<br>'+
-				'<b>Phone:</b>(510) 451-4000</b>' +
+				'<b>Phone:</b>(510) 451-4000</b><br></br>' +
+				'<a href="https://resweb.passkey.com/Resweb.do?mode=welcome_gi_new&groupID=20984319" class="btn btn-success btn-xs">Make a Reservation</a>'+
 			'</div>'+
 		'</div>';
 
